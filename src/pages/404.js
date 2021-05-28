2ee604bea6380821026fb4389dbb114ec347cea0
Akke <atomwar.box@gmail.com>
2021-05-28 20:31:42 +0200

    feat: new 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,13 @@
+import styles from "../styles/404.module.sass";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+    return (
+        <div className={styles.pageNotFound}>
+            <h1 className={styles.header}>404</h1>
+            <div className={styles.description}>
+                <p>The page you're looking for couldn't be found — did you check under the carpet?</p>
+                <div className={styles.return}><a href="/">Return to the homepage.</a></div>
+            </div>
+        </div>
+    );
 }
