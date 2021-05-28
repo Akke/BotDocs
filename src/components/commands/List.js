bf9bab79ad2fad9e115957c7fef26d4d08763932
Yimura <andreas.maerten@scarlet.be>
2021-05-28 14:08:06 +0200

    refactor(Commands/List): Add requested changes
    - Changed undefined to null
    - Removed unused arg

--- src/components/commands/List.js
+++ src/components/commands/List.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 import Search from "./Search";
 import styles from "./_commands.module.sass";
 
-export default function List({ commands, mode, colors, isCategoryActive, currentCategory, onSearch, t }) {
+export default function List({ commands, mode, colors, isCategoryActive, onSearch, t }) {
     return (
         <div className={`${styles.commands} ${styles["commands--" + mode]}`}>
-            <Search onSearch={onSearch} isCategoryActive={isCategoryActive} currentCategory={isCategoryActive ? commands[0].category : undefined} t={t} />
+            <Search onSearch={onSearch} isCategoryActive={isCategoryActive} currentCategory={isCategoryActive ? commands[0].category : null} t={t} />
 
             {commands.map((command, i) => (
                 <Link href={command.url.full} shallow={true} key={i}>
